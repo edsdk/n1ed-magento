117221d8dba11e2d8499d54db96588dbace341f2
Avgustine <ilia@kandaurov.su>
2020-12-03 15:26:36 +0300

    Namespace fixes

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
   \Magento\Framework\Component\ComponentRegistrar::MODULE,
-  'Avgustine_Wysiwyg',
+  'EdSDK_N1ED',
   __DIR__
 );
 
