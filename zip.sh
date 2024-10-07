e42ce916705dd330d9e97a58a6e1e16e0118a5b1
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2024-10-07 16:26:25 +0400

    -

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p zip
-zip -r zip/edsdk_n1ed-magento-1.0.7.zip . -x '.git/*' 'zip.sh' -x 'zip/*' '.gitignore'
+zip -r zip/edsdk_n1ed-magento-1.0.9.zip . -x '.git/*' 'zip.sh' -x 'zip/*' '.gitignore'
