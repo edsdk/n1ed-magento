cce854fbea5dde6936dcc866e29f60bb11ebf922
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2024-10-23 13:53:54 +0400

    Zip script has been updated

--- zip.sh
+++ zip.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 mkdir -p zip
-zip -r zip/edsdk_n1ed-magento-1.0.9.zip . -x '.git/*' 'zip.sh' -x 'zip/*' '.gitignore'
+rm -f zip/edsdk_n1ed-magento-1.0.11.zip
+zip -r zip/edsdk_n1ed-magento-1.0.11.zip . -x '.git/' -x '.git/*' -x '.idea/' -x '.idea/*' 'zip.sh' -x 'zip/*' '.gitignore'
+#unzip -l zip/edsdk_n1ed-magento-1.0.11.zip
